217c91832aad68c07629b1a39283698da995d7f1
Hj <85759350@qq.com>
2026-06-25 10:50:03 +0800

    fix: 删除 recommend 包与 content 重名的 Favorite/UserBehavior 脚手架（Bean 名冲突致 dev 启动失败）

--- src/main/java/com/petshop/recommend/mapper/UserBehaviorMapper.java
+++
@@ -1,9 +0,0 @@
-package com.petshop.recommend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.petshop.recommend.entity.UserBehavior;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserBehaviorMapper extends BaseMapper<UserBehavior> {
-}
