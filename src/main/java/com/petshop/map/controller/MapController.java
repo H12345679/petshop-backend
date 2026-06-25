094417f19fefba4f2ec1c707f5cee94ac4c248c5
深山闻鹿鸣 <15690404+shenshan114514@user.noreply.gitee.com>
2026-06-25 15:15:09 +0800

    完成地图接口周边搜索、路线搜索、从定位到目标商店导航

--- src/main/java/com/petshop/map/controller/MapController.java
+++ src/main/java/com/petshop/map/controller/MapController.java
@@ -1,16 +1,21 @@
 package com.petshop.map.controller;
 
 import com.petshop.common.Result;
+import com.petshop.map.dto.MapDistanceQuery;
+import com.petshop.map.dto.MapDistanceResponse;
 import com.petshop.map.dto.MapShopQuery;
 import com.petshop.map.dto.MapShopResponse;
 import com.petshop.map.service.MapShopService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Min;
 import java.util.List;
 
 @Api(tags = "D-地图LBS")
@@ -29,5 +34,19 @@ public class MapController {
     @GetMapping("/shops")
     public Result<List<MapShopResponse>> nearbyShops(@Validated MapShopQuery query) {
         return Result.success(mapShopService.findNearbyShops(query));
+    }
+
+    @ApiOperation("获取单个商店坐标信息（地图弹窗用）")
+    @GetMapping("/shops/{id}")
+    public Result<MapShopResponse> shopLocation(
+            @ApiParam(value = "商店ID", required = true)
+            @PathVariable @Min(value = 1, message = "商店ID必须大于0") Long id) {
+        return Result.success(mapShopService.getShopLocation(id));
+    }
+
+    @ApiOperation("计算两点距离与预估时长")
+    @GetMapping("/distance")
+    public Result<MapDistanceResponse> calcDistance(@Validated MapDistanceQuery query) {
+        return Result.success(mapShopService.calcDistance(query));
     }
 }
--- src/main/java/com/petshop/map/service/MapShopService.java
+++ src/main/java/com/petshop/map/service/MapShopService.java
@@ -1,5 +1,7 @@
 package com.petshop.map.service;
 
+import com.petshop.map.dto.MapDistanceQuery;
+import com.petshop.map.dto.MapDistanceResponse;
 import com.petshop.map.dto.MapShopQuery;
 import com.petshop.map.dto.MapShopResponse;
 
@@ -8,4 +10,8 @@ import java.util.List;
 public interface MapShopService {
 
     List<MapShopResponse> findNearbyShops(MapShopQuery query);
+
+    MapShopResponse getShopLocation(Long shopId);
+
+    MapDistanceResponse calcDistance(MapDistanceQuery query);
 }
